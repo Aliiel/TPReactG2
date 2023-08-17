03ac7eb97bd449ca4c9074229082264c802a2dce
Aliiel <leila.mouaci@gmail.com>
2023-08-17 14:27:01 +0200

    structure navbar

---
+++ TPReactG2/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link} from 'react-router-dom';
+
+
+
+const Navbar = () => {
+
+    return (
+
+        <nav>
+
+            <Link to = "/Profile">Profile</Link>
+            <Link to = "/Cart">Panier</Link>
+            <Link to = "/Login">Se connecter</Link>
+            <Link to = "/SignUp">S'inscrire</Link>
+
+        </nav>
+    )
+}
+
+
+export default Navbar
