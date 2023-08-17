1bd4d3622960184a4bbb833fe2b1d5ae029a3cf3
QuentoVich <quentin.olivier6@outlook.fr>
2023-08-17 16:00:58 +0200

    view products via API

--- TPReactG2/src/components/ProductCard.jsx
+++ TPReactG2/src/components/ProductCard.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const ProductCard = ({ product} ) => {
   return (
     <div className='Produit'> 
-    { product.title }
+   
     <img src={ product.image }></img> 
-    { product.price}
+     <p> { product.title }  </p>
+     <p>   { product.price} € </p>
     
     </div>
   )
