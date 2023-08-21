0a94240581913db56279b0a076adc293c2792371
Aliiel <leila.mouaci@gmail.com>
2023-08-21 16:13:38 +0200

    avancement header

--- TPReactG2/src/components/Header.jsx
+++ TPReactG2/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../assets/images/MODB.png'
+import LogoPanier from '../assets/images/logo-panier.png'
+import LogoRecherche from '../assets/images/logo-loupe.png'
 
 
 
@@ -12,14 +14,25 @@ const Header = () => {
 
         <nav>
 
-            <Link to = "/Home"><img src = {Logo}></img></Link>
-            <Link to = "/Profile">Profile</Link>
-            <Link to = "/Cart">Panier</Link>
-            <div>
-            Rechercher :<input></input>
+            <div className = 'logo'>
+            <Link to = "/Home"><img src = {Logo} style = {{width : '100px'}}></img></Link>
             </div>
-            <Link to = "/Login">Se connecter</Link>
-            <Link to = "/Signup">S'inscrire</Link>
+            
+            <div className = 'recherche'> 
+            <input style = {{width : '400px', height : '25px'}} placeholder='Rechercher'></input>
+            </div>
+
+            <div className = 'connecter'>
+            <Link to = "/Login">Connectez-vous</Link>
+            </div>
+
+            <div className = 'inscrire'>
+            <Link to = "/Signup">Inscrivez-vous</Link>
+            </div>
+
+            <div className = 'panier'>
+                <Link to = "/Cart"><img src = {LogoPanier} style = {{width : '70px'}}></img></Link>
+            </div>
             
         </nav>
     )
