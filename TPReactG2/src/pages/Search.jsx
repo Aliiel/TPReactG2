91ff89b9a4a7f6aafd7738780083d80811f79921
QuentoVich <quentin.olivier6@outlook.fr>
2023-11-06 09:25:58 +0100

    maj

---
+++ TPReactG2/src/pages/Search.jsx
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Products extends Component{
+
+// render(){
+
+// return(
+//     <div className='Produit'>
+//         <p prix ="120 euros"> Arte </p>
+//     </div>
+    
+
+// )
+
+// }
+
+// }
+
+// export default Products; 
